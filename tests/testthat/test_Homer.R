182e9f52cd19b4a5ca10fafeb27e74a2ddf7902f
mbstadler <stadler.michael@gmail.com>
2019-04-30 18:04:18 +0200

    edits to Homer unit tests

--- tests/testthat/test_Homer.R
+++ tests/testthat/test_Homer.R
@@ -4,14 +4,14 @@ test_that("findHomer() works properly", {
     res <- findHomer("I-do-not-exist")
     expect_true(is.na(res))
 
-    res <- findHomer("NAMESPACE", dirs = system.file(package = "lisa"))
+    res <- findHomer("resL.rds", dirs = system.file("extdata", package = "lisa"))
     expect_true(file.exists(res))
 })
 
 test_that("dumpJaspar() works properly", {
     tmp1 <- tempfile()
 
-    expect_error(dumpJaspar(filename = file.path(system.file(package = "lisa"), "NAMESPACE")))
+    expect_error(dumpJaspar(filename = system.file("extdata", "resL.rds", package = "lisa")))
     expect_error(dumpJaspar(filename = tmp1, relScoreCutoff = "error"))
     expect_true(dumpJaspar(filename = tmp1, opts = list(ID = c("MA0006.1", "MA0007.3"))))
 
@@ -26,11 +26,11 @@ test_that("prepareHomer() works properly", {
     tmp1 <- tempfile()
     dir.create(tmp1)
     tmp2 <- tempfile()
-    fname <- file.path(system.file(package = "lisa"), "NAMESPACE")
+    fname <- system.file("extdata", "resL.rds", package = "lisa")
 
     expect_error(prepareHomer(gr = gr, b = b, genomedir = "genomedir", outdir = tmp1,
                               motifFile = fname, homerfile = fname, regionsize = "given", Ncpu = 2))
-    expect_error(prepareHomer(gr = gr, b = bF[1:10], genomedir = "genomedir", outdir = tmp2,
+    expect_error(prepareHomer(gr = as(gr, "data.frame"), b = bF[1:10], genomedir = "genomedir", outdir = tmp2,
                               motifFile = fname, homerfile = fname, regionsize = "given", Ncpu = 2))
     expect_error(prepareHomer(gr = gr, b = bF, genomedir = "genomedir", outdir = tmp2,
                               motifFile = "error", homerfile = fname, regionsize = "given", Ncpu = 2))
