e949226f2157fd61eae54e834c8b383de7dd40b6
mbstadler <stadler.michael@gmail.com>
2019-04-30 08:45:28 +0200

    fixed typos

--- R/stability_selection.R
+++ R/stability_selection.R
@@ -3,39 +3,39 @@
 #' @importFrom TFBSTools PWMatrixList
 NULL
 
-#'@title Randomized Lasso 
+#'@title Randomized Lasso
 #'
-#'@description This function performs randomized lasso using the \code{glmnet} package. The 
+#'@description This function performs randomized lasso using the \code{glmnet} package. The
 #'function present in the \code{stabs} package that runs the lasso version was adapted for
 #'the randomized lasso here. Randmized lasso stability selection uses this function repeatedly
-#'to select predictors. 
+#'to select predictors.
 #'
 #'
-#'@param x the predictor matrix. 
+#'@param x the predictor matrix.
 #'@param y the response vector.
 #'@param q the average number of selected variables.
-#'@param weakness parameter to apply randomized lasso. 
-#'@param type parameter from \code{lars.lasso} function in \code{stabs}. It is a character vector specifying 
-#'how much the PFER should be controlled. If type is "conservative" then the number of selected variables per 
+#'@param weakness parameter used randomized lasso (see details).
+#'@param type parameter from \code{lars.lasso} function in \code{stabs}. It is a character vector specifying
+#'how much the PFER should be controlled. If type is "conservative" then the number of selected variables per
 #'subsample is <= q. If type is "anticonservative" then the number of selected variables per subsample is >= q.
 #'@param ... additional parameters for \code{glmnet}.
 #'
 #'@return the regression output which consists of a list of length 2. The list contains the following:
 #'\itemize{
-#'  \item selected - a logical vector of length equal to the total number of predictors. The predictors that were chosen have a value of TRUE. 
-#'  \item path - a logical matrix containing the regularization steps as columns and the predictors as rows. An entry of TRUE indicates selection. 
+#'  \item selected - a logical vector of length equal to the total number of predictors. The predictors that were chosen have a value of TRUE.
+#'  \item path - a logical matrix containing the regularization steps as columns and the predictors as rows. An entry of TRUE indicates selection.
 #'}
 #'
 #'
 #'@details This function is identical to \code{glmnet.lasso} from the \code{stabs} package. The only addition is the weakness parameter
 #'which has been added when calling the \code{glmnet} function by setting penalty.factor = 1/runif(ncol(x), weakness, 1)
-#'where ncol(x) is the number of predictors. 
+#'where ncol(x) is the number of predictors.
 #'
 #'@seealso \code{\link[stabs]{glmnet.lasso}} and \code{\link[glmnet]{glmnet}}
 #'
 #'@author Dania Machlab
 #'@export
-glmnet.randomized_lasso <- function (x, y, q, weakness=1, type = c("conservative", "anticonservative"), ...) {
+glmnet.randomized_lasso <- function(x, y, q, weakness=1, type = c("conservative", "anticonservative"), ...) {
   if (!requireNamespace("glmnet", quietly = TRUE))
     stop("Package ", sQuote("glmnet"), " needed but not available")
   if (is.data.frame(x)) {
@@ -66,18 +66,18 @@ glmnet.randomized_lasso <- function (x, y, q, weakness=1, type = c("conservative
 #'@title Randomized Lasso Stability Selection
 #'
 #'@description This function runs randomized lasso stability selection as presented by Meinshausen
-#'and Bühlmann (2010) and with the improved error bounds introduced by Shah and Samworth (2013). The function 
+#'and Bühlmann (2010) and with the improved error bounds introduced by Shah and Samworth (2013). The function
 #'uses the \code{stabsel} function from the \code{stabs} package, but implements the randomized lasso version.
 #'
 #'@param x the predictor matrix.
 #'@param y the response vector.
-#'@param weakness parameter that can take a value between 0 and 1. It affects how strict the method will be in 
-#'selecting predictors. It is set to 0.8 by default. The closer it goes to 0 the more stringent the selection. A 
+#'@param weakness parameter that can take a value between 0 and 1. It affects how strict the method will be in
+#'selecting predictors. It is set to 0.8 by default. The closer it goes to 0 the more stringent the selection. A
 #'weakness value of 1 is identical to performing lasso stability selection (not the randomized version).
 #'@param cutoff value between 0 and 1 which is the cutoff for the selection probability. Any variable
 #'with a selection probability that is higher than the set cutoff will be selected. It is set to 0.8 by default.
-#'@param PFER the absolute number of false positives that we allow for in the final list of 
-#'selected variables. This comes from the Meinshausen and Bühlmann paper.  
+#'@param PFER the absolute number of false positives that we allow for in the final list of
+#'selected variables. For details see Meinshausen and Bühlmann (2010).
 #'@param ... additional parameters for \code{glmnet}.
 #'
 #'@return a \code{stabsel} object. It contains the following:
@@ -86,7 +86,7 @@ glmnet.randomized_lasso <- function (x, y, q, weakness=1, type = c("conservative
 #'  are predictors.
 #'  \item selected - a vector containing the selected predictors.
 #'  \item max - maximum of selection probabilities.
-#'  \item cutoff - the selection probability cutoff. 
+#'  \item cutoff - the selection probability cutoff.
 #'  \item q - average number of selected variables used.
 #'  \item PFER - the realized upper bound for the per-family error rate (number of falsely selected predictors among the group of selected predictors).
 #'  \item specifiedPFER - the set upper bound for the per-family error rate (number of falsely selected predictors among the group of selected predictors).
@@ -97,16 +97,16 @@ glmnet.randomized_lasso <- function (x, y, q, weakness=1, type = c("conservative
 #'  \item call - the call.
 #'}
 #'
-#'@details Randomized lasso stability selection runs randomized lasso several times on subsamples of 
+#'@details Randomized lasso stability selection runs randomized lasso several times on subsamples of
 #'the response variable and predictor matrix (stability selection). N/2 elements from the response variable are
-#'randomly chosen, where N is the length of the vector. Their corresponsing section of the predictor matrix is 
-#'also chosen, and the \code{glmnet.randomized_lasso} function is applied. This is done multiple times, and 
-#'results in selection probabilities for each predictor. The probability of a specific predictor is the number of 
-#'times it was selected divided by the total number of subsamples that were done (total number of times the 
-#'regression was performed). 
+#'randomly chosen, where N is the length of the vector. Their corresponsing section of the predictor matrix is
+#'also chosen, and the \code{glmnet.randomized_lasso} function is applied. This is done multiple times, and
+#'results in selection probabilities for each predictor. The probability of a specific predictor is the number of
+#'times it was selected divided by the total number of subsamples that were done (total number of times the
+#'regression was performed).
 #'
-#'We make use of the \code{stabs} package that implements lasso stability selection, and adapt it to run the 
-#'randomized lasso stability selection. The output is an object of type \code{stabsel}. 
+#'We make use of the \code{stabs} package that implements lasso stability selection, and adapt it to run the
+#'randomized lasso stability selection. The output is an object of type \code{stabsel}.
 #'
 #'@seealso \code{\link[stabs]{stabsel}}
 #'
@@ -118,22 +118,22 @@ glmnet.randomized_lasso <- function (x, y, q, weakness=1, type = c("conservative
 #'@author Dania Machlab
 #'@export
 randomized_stabsel <- function(x=x, y=y, weakness=0.8, cutoff=0.8, PFER=2, ...) {
-  stabs::stabsel(x=x, y=y, fitfun=glmnet.randomized_lasso, args.fitfun=list(weakness=weakness), cutoff=cutoff, PFER=PFER, ...)
+  stabs::stabsel(x = x, y = y, fitfun = glmnet.randomized_lasso, args.fitfun = list(weakness = weakness), cutoff = cutoff, PFER = PFER, ...)
 }
 
 
 
 
-#'@title TFBS Matrix 
+#'@title TFBS Matrix
 #'
-#'@description This function takes in a \code{GRanges} that is the output of \code{findMotifHits} and returns a 
+#'@description This function takes in a \code{GRanges} that is the output of \code{findMotifHits} and returns a
 #'matrix contaiting the number of transcription factor bisnding sites (TFBS) per motif across a given set of genomic ranges.
 #'
 #'@param TFBS_gr the output of \code{findMotifHits} contaiting the TF binding locations across specified genomic regions.
-#'@param subject_gr a \code{GRanges} object showing the positions of the geominc regions that have been scanned for the TFs. 
-#'This corresponds to the \code{GRanges} \code{subject} parameter used in the \code{findMorifHits} function.
+#'@param subject_gr a \code{GRanges} object showing the positions of the geominc regions that have been scanned for the TFs.
+#'This corresponds to the \code{GRanges} \code{subject} parameter used in the \code{findMotifHits} function.
 #'@param PWMs \code{PWMatrixList} or \code{PWMatrix} object of the used TFs. This corresponds to the \code{query}
-#'parameter used in the \code{findMorifHits} function.
+#'parameter used in the \code{findMotifHits} function.
 #'@param Ncpu number of CPUs to use (set to 1 by default).
 #'
 #'@return a matrix containing the number of binding sites each TF has across the genomic regions.
@@ -141,30 +141,30 @@ randomized_stabsel <- function(x=x, y=y, weakness=0.8, cutoff=0.8, PFER=2, ...) 
 #'@author Dania Machlab
 #'@export
 get_numberOfTFBS_perSeqName <- function(TFBS_gr, subject_gr, PWMs, Ncpu=1L) {
-  
+
   # TODO make sure PWM names and peak names are  unique
   # motif names instead of gene?
-  
+
   ## checks
-  stopifnot(class(TFBS_gr)=="GRanges")
-  stopifnot(all(colnames(as.data.frame(TFBS_gr))==c("seqnames", "start", "end", "width", "strand", "matchedSeq", "pwmname", "score")))
-  stopifnot(class(PWMs)=="PWMatrixList"| class(PWMs)=="PWMatrix")
-  if(class(PWMs)=="PWMatrix"){PWMs <- TFBSTools::PWMatrixList(PWMs)}
-  # if(!all(TFBS_gr$pwmname%in%sapply(PWMs, function(x){name(x)}))){stop("PWMs missing motifs found in TFBS_gr")}
+  stopifnot(class(TFBS_gr) == "GRanges")
+  stopifnot(all(colnames(as.data.frame(TFBS_gr)) == c("seqnames", "start", "end", "width", "strand", "matchedSeq", "pwmname", "score")))
+  stopifnot(class(PWMs) == "PWMatrixList" | class(PWMs) == "PWMatrix")
+  if (class(PWMs) == "PWMatrix") {PWMs <- TFBSTools::PWMatrixList(PWMs)}
+  # if(!all(TFBS_gr$pwmname %in% sapply(PWMs, function(x){name(x)}))) {stop("PWMs missing motifs found in TFBS_gr")}
 
   ## for each motif count number of TFBS per seqName
   seqs <- as.character(seqnames(TFBS_gr))
   TFs <- as.character(TFBS_gr$pwmname)
   s <- split(TFs, seqs)
   l <- lapply(s, function(x){table(x)})
-  
+
   ## output full matrix
-  if(is.null(names(subject_gr))) {
+  if (is.null(names(subject_gr))) {
     names(subject_gr) <- paste0("row_", seq(from = 1, to = length(subject_gr), by = 1))
   }
 
   ## rbind vectors
-  m <- do.call(rbind, mclapply(mc.cores = Ncpu, X = l, FUN = function(x){
+  m <- do.call(rbind, mclapply(mc.cores = Ncpu, X = l, FUN = function(x) {
     full_motif_vec <- numeric(length(PWMs))
     names(full_motif_vec) <- sapply(PWMs, function(x){name(x)})
     df <- as.data.frame(x)
@@ -172,15 +172,15 @@ get_numberOfTFBS_perSeqName <- function(TFBS_gr, subject_gr, PWMs, Ncpu=1L) {
     full_motif_vec[motifs] <- df$Freq
     full_motif_vec
   }))
-  
+
   ## order to match subject_gr
-  subject_peaks <- names(subject_gr)[names(subject_gr)%in%rownames(m)] # remove peaks that have 0 TFBS in all columns
+  subject_peaks <- names(subject_gr)[names(subject_gr) %in% rownames(m)] # remove peaks that have 0 TFBS in all columns
   o <- match(subject_peaks, rownames(m))
   m <- m[o, ]
 
   ## return matrix
   m
-  
+
 }
 
 
