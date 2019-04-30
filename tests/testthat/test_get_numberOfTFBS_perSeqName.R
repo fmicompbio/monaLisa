a49104544b8aad8b635ec1372ca18bc1029700c2
mbstadler <stadler.michael@gmail.com>
2019-04-30 10:33:09 +0200

    reformat unit test

--- tests/testthat/test_get_numberOfTFBS_perSeqName.R
+++ tests/testthat/test_get_numberOfTFBS_perSeqName.R
@@ -2,8 +2,10 @@ test_that("get_numberOfTFBS_perSeqName() works properly", {
 
   # create data set
   pwms <- TFBSTools::getMatrixSet(JASPAR2018::JASPAR2018,
-                                  list(matrixtype = "PWM", tax_group = "vertebrates", ID = c("MA0095.2", "MA0701.1", "MA0741.1")))
-  sbj <- GRanges(seqnames = rep("chr1", 2), strand = "*", ranges = IRanges(start = c(90000000, 100000000), width = 5000))
+                                  list(matrixtype = "PWM", tax_group = "vertebrates",
+                                       ID = c("MA0095.2", "MA0701.1", "MA0741.1")))
+  sbj <- GenomicRanges::GRanges(seqnames = rep("chr1", 2), strand = "*",
+                                ranges = IRanges::IRanges(start = c(9e7, 1e8), width = 5000))
   tf_gr <- findMotifHits(query = pwms, subject = sbj, min.score = 8.0, method = "matchPWM",
                          genome = BSgenome.Mmusculus.UCSC.mm10::BSgenome.Mmusculus.UCSC.mm10, Ncpu = 1L)
 
