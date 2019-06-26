3133a41e060ae9f73cb4f84ee2a9138753014a41
mbstadler <stadler.michael@gmail.com>
2019-06-26 17:27:00 +0200

    added basic documentation and required imports for getKmerFreq

--- R/motif_enrichment_kmers.R
+++ R/motif_enrichment_kmers.R
@@ -1,24 +1,54 @@
-getKmerFreq <- function(seq, kmerLen=4, MMorder=1, pseudoCount=0) {
-    #seq is DNAStringSet
-    #only works if Markov order at least twice smaller than kmerLen
-    require(Biostrings)
-    require(tidyverse)
+#' @title Calculate observed and expected k-mer frequencies
+#'
+#' @description Given a set of sequences, calculate observed and expected k-mer
+#'   frequencies. Expected frequencies are based on a Markov model of order
+#'   \code{MMorder}.
+#'
+#' @param seq Set of sequences, either a \code{character} vector or a
+#'   \code{\link{DNAStringSet}}.
+#' @param kmerLen A \code{numeric} scalar giving the k-mer length.
+#' @param MMorder A \code{numeric} scalar giving the order of the Markov model
+#'   used to calculate the expected frequencies.
+#' @param pseudoCount A \code{numeric} scalar - will be added to the observed
+#'   counts for each k-mer to avoid zero counts.
+#'
+#' @importFrom Biostrings DNAStringSet oligonucleotideFrequency
+#' @importFrom tidyr %>%
+#'
+#' @export
+getKmerFreq <- function(seq, kmerLen = 4, MMorder = 1, pseudoCount = 1) {
+    if (is.character(seq))
+        seq <- DNAStringSet(seq)
+    stopifnot(is(seq, "DNAStringSet"))
+    stopifnot(exprs = {
+        is.numeric(kmerLen)
+        length(kmerLen) == 1L
+        round(kmerLen, 0L) == kmerLen
+    })
+    stopifnot(exprs = {
+        is.numeric(MMorder)
+        length(MMorder) == 1L
+        round(MMorder, 0L) == MMorder
+        MMorder < kmerLen - 1L
+    })
+
     #kmer frequencies
     kmerFreq <- oligonucleotideFrequency(seq, width = kmerLen) %>% colSums
 
     #calculate probabilities for bg model (with a pseudocount of 1)
-    p_long <- oligonucleotideFrequency(seq, width=MMorder+1) %>% {colSums(. + pseudoCount)} %>% {./sum(.)}
-    p_short <- oligonucleotideFrequency(seq, width=MMorder) %>% {colSums(. + pseudoCount)} %>% {./sum(.)}
+    p_long  <- oligonucleotideFrequency(seq, width = MMorder + 1) %>% {colSums(. + pseudoCount)} %>% {./sum(.)}
+    p_short <- oligonucleotideFrequency(seq, width = MMorder)     %>% {colSums(. + pseudoCount)} %>% {./sum(.)}
 
     log2pMM <- sapply(names(kmerFreq), function(current.kmer){
-        ii_long <- sapply(1:(nchar(current.kmer)-(MMorder)), function(i){
+        ii_long <- sapply(1:(nchar(current.kmer) - MMorder), function(i) {
             subseq(current.kmer, start = i, width = MMorder + 1)
         })
-        ii_short <- sapply(2:(nchar(current.kmer)-MMorder), function(i){
+        ii_short <- sapply(2:(nchar(current.kmer) - MMorder), function(i) {
             subseq(current.kmer, start = i, width = MMorder)
         })
         sum(log2(p_long[ii_long])) - sum(log2(p_short[ii_short]))
     })
     kmerFreqMM <- (2**log2pMM)*sum(kmerFreq)
+
     data.frame(kmerFreq=kmerFreq, kmerFreqMM=kmerFreqMM)
 }
