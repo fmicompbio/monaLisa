dad60a482c952f68865f50a57c9093eab0e7bdca
mbstadler <stadler.michael@gmail.com>
2019-06-27 08:11:43 +0200

    code cleanup/reformatting

--- R/motif_enrichment_kmers.R
+++ R/motif_enrichment_kmers.R
@@ -18,6 +18,7 @@
 #'
 #' @export
 getKmerFreq <- function(seqs, kmerLen = 4, MMorder = 2, pseudoCount = 1) {
+    ## pre-flight checks
     if (is.character(seqs))
         seqs <- DNAStringSet(seqs)
     stopifnot(is(seqs, "DNAStringSet"))
@@ -34,20 +35,26 @@ getKmerFreq <- function(seqs, kmerLen = 4, MMorder = 2, pseudoCount = 1) {
         MMorder < kmerLen
     })
 
-    #kmer frequencies
+    ## observed k-mer frequencies
     kmerFreq <- oligonucleotideFrequency(seqs, width = kmerLen) %>% colSums
 
-    #calculate probabilities for bg model (with a pseudocount)
-    lp_long  <- log2(oligonucleotideFrequency(seqs, width = MMorder)      %>% {colSums(. + pseudoCount)} %>% {./sum(.)})
-    lp_short <- log2(oligonucleotideFrequency(seqs, width = MMorder - 1L) %>% {colSums(. + pseudoCount)} %>% {./sum(.)})
-
-    log2pMM <- sapply(names(kmerFreq), function(current.kmer){
+    ## expected k-mer frequencies (log2-probabilities with a pseudocount)
+    lp_long  <- log2(oligonucleotideFrequency(seqs, width = MMorder) %>%
+                     { colSums(. + pseudoCount) } %>%
+                     { . / sum(.) })
+    lp_short <- log2(oligonucleotideFrequency(seqs, width = MMorder - 1L) %>%
+                     { colSums(. + pseudoCount) } %>%
+                     { . / sum(.) })
+    log2pMM <- sapply(names(kmerFreq), function(current.kmer) {
         n <- nchar(current.kmer) - MMorder + 1L
-        ii_long <- substr(rep(current.kmer, n), start = 1:n, stop = 0:(n - 1L) + MMorder)
-        ii_short <- substr(rep(current.kmer, n - 1L), start = 2:n, stop = 1:(n - 1L) + MMorder - 1L)
+        ii_long <- substr(rep(current.kmer, n),
+                          start = 1:n, stop = 0:(n - 1L) + MMorder)
+        ii_short <- substr(rep(current.kmer, n - 1L),
+                           start = 2:n, stop = 1:(n - 1L) + MMorder - 1L)
         sum(lp_long[ii_long]) - sum(lp_short[ii_short])
     })
-    kmerFreqMM <- (2**log2pMM)*sum(kmerFreq)
+    kmerFreqMM <- (2 ** log2pMM) * sum(kmerFreq)
 
+    ## return results
     data.frame(kmerFreq=kmerFreq, kmerFreqMM=kmerFreqMM)
 }
