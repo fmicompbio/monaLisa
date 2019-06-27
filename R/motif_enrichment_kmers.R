df5c99760eab506523ef8557076637a24d9d126e
mbstadler <stadler.michael@gmail.com>
2019-06-27 08:06:15 +0200

    take log2 out of the loop

--- R/motif_enrichment_kmers.R
+++ R/motif_enrichment_kmers.R
@@ -38,14 +38,14 @@ getKmerFreq <- function(seqs, kmerLen = 4, MMorder = 2, pseudoCount = 1) {
     kmerFreq <- oligonucleotideFrequency(seqs, width = kmerLen) %>% colSums
 
     #calculate probabilities for bg model (with a pseudocount)
-    p_long  <- oligonucleotideFrequency(seqs, width = MMorder)      %>% {colSums(. + pseudoCount)} %>% {./sum(.)}
-    p_short <- oligonucleotideFrequency(seqs, width = MMorder - 1L) %>% {colSums(. + pseudoCount)} %>% {./sum(.)}
+    lp_long  <- log2(oligonucleotideFrequency(seqs, width = MMorder)      %>% {colSums(. + pseudoCount)} %>% {./sum(.)})
+    lp_short <- log2(oligonucleotideFrequency(seqs, width = MMorder - 1L) %>% {colSums(. + pseudoCount)} %>% {./sum(.)})
 
     log2pMM <- sapply(names(kmerFreq), function(current.kmer){
         n <- nchar(current.kmer) - MMorder + 1L
         ii_long <- substr(rep(current.kmer, n), start = 1:n, stop = 0:(n - 1L) + MMorder)
         ii_short <- substr(rep(current.kmer, n - 1L), start = 2:n, stop = 1:(n - 1L) + MMorder - 1L)
-        sum(log2(p_long[ii_long])) - sum(log2(p_short[ii_short]))
+        sum(lp_long[ii_long]) - sum(lp_short[ii_short])
     })
     kmerFreqMM <- (2**log2pMM)*sum(kmerFreq)
 
