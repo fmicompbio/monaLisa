9db0648e53014bf5097793799f51378ad0c547fe
machlabd <dania.machlab@gmail.com>
2020-10-16 10:14:21 +0200

    fix bug in plotMotifDirectionality to also put motif names if all are in the (-) directionality.

--- R/plotting.R
+++ R/plotting.R
@@ -514,12 +514,13 @@ plotMotifDirectionality <- function(stabs_obj = NULL, response = NULL, predictor
     bar <- graphics::barplot(probs, col = cols, border = NA, ylab = "Sel Prob * sign(cor to response)", names.arg = NA, 
                              ylim = c(min(0, range(probs)[1]-abs(0.3*range(probs)[1])), max(1, range(probs[2]+abs(0.3*range(probs)[2])))), ...)
     legend("topright", bty = "n", lty = 1, legend = c("selected", "not selected"), col = c(sel_color, "grey"))
-    if(!(sum(up)==0)&!isEmpty(probs)){
-        graphics::text(x = bar[up], y = probs[up], labels = tf_names[up], col = cols[up], xpd = TRUE, srt=90, adj = 0)
-        if(!(sum(!up)==0)){
-            graphics::text(x = bar[!up], y = probs[!up], labels = tf_names[!up], col = cols[!up], xpd = TRUE, srt=90, adj = 1)
-        }
+    if(!(sum(up)==0)&!isEmpty(probs[up])){
+      graphics::text(x = bar[up], y = probs[up], labels = tf_names[up], col = cols[up], xpd = TRUE, srt=90, adj = 0)
     }
+    if(!(sum(!up)==0&!isEmpty(probs[!up]))){
+      graphics::text(x = bar[!up], y = probs[!up], labels = tf_names[!up], col = cols[!up], xpd = TRUE, srt=90, adj = 1)
+    }
+    
     invisible(TRUE)
     
 }
