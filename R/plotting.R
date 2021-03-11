f60bb6a24bbd09278701e1c1c41d23f3b9213692
mbstadler <stadler.michael@gmail.com>
2021-03-11 18:00:48 +0100

    add missing imports; use gray colors for significance heatmap; simplify code

--- R/plotting.R
+++ R/plotting.R
@@ -207,6 +207,7 @@ plotBinScatter <- function(x, y, b,
 #' @param width The width (in inches) of each individual heatmap, without legend.
 #' @param col.enr Colors used for enrichment heatmap.
 #' @param col.sig Colors used for significance hetmaps (P values and FDR).
+#' @param col.gc Colors used for motif GC content (for \code{show_motif_GC = TRUE}).
 #' @param maxEnr Cap color mapping at enrichment = \code{maxEnr} (default: 99.5th percentile).
 #' @param maxSig Cap color mapping at -log10 P value or -log10 FDR = \code{maxSig}
 #'     (default: 99.5th percentile).
@@ -226,8 +227,8 @@ plotBinScatter <- function(x, y, b,
 #' @param use_raster \code{TRUE} or \code{FALSE} (default). Passed to \code{use_raster}
 #'     of \code{\link[ComplexHeatmap]{Heatmap}}.
 #'
-#' @details The heatmaps are plotted side-by-side and are created internally using
-#'     the \pkg{ComplexHeatmap} package.
+#' @details The heatmaps are created using the \pkg{ComplexHeatmap} package
+#'     and plotted side-by-side.
 #'
 #'     Each heatmap will be \code{width} inches wide, so the total plot needs a
 #'     graphics device with a width of at least \code{length(which.plots) * width}
@@ -242,19 +243,36 @@ plotBinScatter <- function(x, y, b,
 #' @return A list of \code{ComplexHeatmap::Heatmap} objects.
 #'
 #' @importFrom methods is
+#' @importFrom stats hclust dist quantile
+#' @importFrom TFBSTools Matrix
 #' @importFrom grDevices colorRampPalette
 #' @importFrom S4Vectors metadata
 #' @importFrom SummarizedExperiment assayNames assay rowData
+#' @importFrom ComplexHeatmap HeatmapAnnotation Heatmap add_heatmap
+#' @importFrom grid unit
+#' @importFrom circlize colorRamp2
 #'
 #' @export
-plotMotifHeatmaps <- function(x, which.plots = c("p", "enr", "FDR", "log2enr"), width = 4,
-                              col.enr = c("#053061","#2166AC","#4393C3","#92C5DE","#D1E5F0",
-                                          "#F7F7F7","#FDDBC7","#F4A582","#D6604D","#B2182B","#67001F"),
-                              col.sig = c("#FFF5EB","#FEE6CE","#FDD0A2","#FDAE6B","#FD8D3C",
-                                          "#F16913","#D94801","#A63603","#7F2704"),
-                              maxEnr = NULL, maxSig = NULL, highlight = NULL, cluster = FALSE,
-                              show_dendrogram = FALSE, show_motif_GC = FALSE,
-                              show_seqlogo = FALSE, width.seqlogo = 1.5,
+plotMotifHeatmaps <- function(x,
+                              which.plots = c("p", "enr", "FDR", "log2enr"),
+                              width = 4,
+                              col.enr = c("#053061","#2166AC","#4393C3","#92C5DE",
+                                          "#D1E5F0","#F7F7F7","#FDDBC7","#F4A582",
+                                          "#D6604D","#B2182B","#67001F"),
+                              col.sig = c("#FFFFFF","#F0F0F0","#D9D9D9","#BDBDBD",
+                                          "#969696","#737373","#525252","#252525",
+                                          "#000000"),
+                              col.gc = c("#F7FCF5","#E5F5E0","#C7E9C0","#A1D99B",
+                                         "#74C476","#41AB5D","#238B45","#006D2C",
+                                         "#00441B"),
+                              maxEnr = NULL,
+                              maxSig = NULL,
+                              highlight = NULL,
+                              cluster = FALSE,
+                              show_dendrogram = FALSE,
+                              show_motif_GC = FALSE,
+                              show_seqlogo = FALSE,
+                              width.seqlogo = 1.5,
                               use_raster = FALSE) {
 	stopifnot(exprs = {
 	    is(x, "SummarizedExperiment")
@@ -276,7 +294,7 @@ plotMotifHeatmaps <- function(x, which.plots = c("p", "enr", "FDR", "log2enr"), 
 	})
 	bincols <- attr(getColsByBin(b), "cols")
 	if (is.logical(cluster) && length(cluster) == 1 && cluster[1] == TRUE) {
-	    clres <- stats::hclust(stats::dist(SummarizedExperiment::assay(x, "enr")))
+	    clres <- hclust(dist(assay(x, "enr")))
 	} else if (is.logical(cluster) && length(cluster) == 1 && cluster[1] == FALSE) {
 	    clres <- FALSE
 	} else if (is(cluster, "hclust")) {
@@ -284,12 +302,12 @@ plotMotifHeatmaps <- function(x, which.plots = c("p", "enr", "FDR", "log2enr"), 
 	} else {
 	    stop("'cluster' must be either TRUE, FALSE or an hclust-object.")
 	}
-	hmBin <- ComplexHeatmap::HeatmapAnnotation(df = data.frame(bin = colnames(x)), name = "bin",
-											   col = list(bin = bincols),
-											   show_annotation_name = FALSE,
-											   which = "column", width = grid::unit(width,"inch"),
-											   annotation_height = grid::unit(width / 16, "inch"),
-											   show_legend = FALSE)
+	hmBin <- HeatmapAnnotation(df = data.frame(bin = colnames(x)), name = "bin",
+	                           col = list(bin = bincols),
+	                           show_annotation_name = FALSE,
+	                           which = "column", width = unit(width,"inch"),
+	                           annotation_height = unit(width / 16, "inch"),
+	                           show_legend = FALSE)
 	tmp <- matrix(if (!is.null(highlight)) as.character(highlight) else rep(NA, nrow(x)),
 								ncol = 1, dimnames = list(rownames(x), NULL))
 	hmSeqlogo <- NULL
@@ -297,38 +315,41 @@ plotMotifHeatmaps <- function(x, which.plots = c("p", "enr", "FDR", "log2enr"), 
 	    pfms <- rowData(x)$motif.pfm
 	    maxwidth <- max(sapply(TFBSTools::Matrix(pfms), ncol))
 	    grobL <- lapply(pfms, seqLogoGrob, xmax = maxwidth, xjust = "center")
-	    hmSeqlogo <- ComplexHeatmap::HeatmapAnnotation(
+	    hmSeqlogo <- HeatmapAnnotation(
 	        logo = anno_seqlogo(grobL = grobL, which = "row",
-	                            space = grid::unit(0.5, "mm"), width = grid::unit(width.seqlogo, "inch")),
+	                            space = unit(0.5, "mm"),
+	                            width = unit(width.seqlogo, "inch")),
 	        show_legend = FALSE, show_annotation_name = FALSE, which = "row")
 	}
-	hmMotifs <- ComplexHeatmap::Heatmap(matrix = tmp, name = "names",
-										width = grid::unit(if (!is.null(highlight)) .2 else 0, "inch"),
-										na_col = NA, col = c("TRUE" = "green3", "FALSE" = "white"),
-										cluster_rows = clres, show_row_dend = show_dendrogram, cluster_columns = FALSE,
-										show_row_names = TRUE, row_names_side = "left",
-										show_column_names = FALSE, show_heatmap_legend = FALSE,
-										left_annotation = hmSeqlogo)
+	hmMotifs <- Heatmap(matrix = tmp, name = "names",
+	                    width = unit(if (!is.null(highlight)) .2 else 0, "inch"),
+	                    na_col = NA, col = c("TRUE" = "green3", "FALSE" = "white"),
+	                    cluster_rows = clres, show_row_dend = show_dendrogram,
+	                    cluster_columns = FALSE, show_row_names = TRUE,
+	                    row_names_side = "left", show_column_names = FALSE,
+	                    show_heatmap_legend = FALSE, left_annotation = hmSeqlogo)
 
-	assayNameMap1 <- c(p = "P value", FDR = "FDR", enr = "enrichment", log2enr = "log2 enrichment")
+	assayNameMap1 <- c(p = "P value", FDR = "FDR", enr = "enrichment",
+	                   log2enr = "log2 enrichment")
 	assayNameMap2 <- c(p = "P value (-log10)", FDR = "FDR (-log10)",
-	                   enr = "enrichment (o-e)/sqrt(e)", log2enr = "enrichment (log2)")
+	                   enr = "enrichment (o-e)/sqrt(e)",
+	                   log2enr = "enrichment (log2)")
 	L <- list(labels = hmMotifs)
 	if (show_motif_GC) {
-	    tmp <- as.matrix(SummarizedExperiment::rowData(x)[, "motif.percentGC", drop = FALSE])
-	    gccols <- c("#F7FCF5","#E5F5E0","#C7E9C0","#A1D99B","#74C476","#41AB5D","#238B45","#006D2C","#00441B")
-	    hmPercentGC <- ComplexHeatmap::Heatmap(matrix = tmp, name = "Percent G+C",
-	                                           width = grid::unit(0.2, "inch"), na_col = NA,
-	                                           col = circlize::colorRamp2(breaks = c(0, seq(20, 80, length.out = 254), 100),
-	                                                                      colors = colorRampPalette(gccols)(256)),
-	                                           cluster_rows = FALSE, cluster_columns = FALSE,
-	                                           show_row_names = FALSE, show_column_names = FALSE,
-	                                           show_heatmap_legend = TRUE, heatmap_legend_param = list(color_bar = "continuous"),
-	                                           use_raster = use_raster)
+	    tmp <- as.matrix(rowData(x)[, "motif.percentGC", drop = FALSE])
+	    hmPercentGC <- Heatmap(matrix = tmp, name = "Percent G+C",
+	                           width = unit(0.2, "inch"), na_col = NA,
+	                           col = colorRamp2(breaks = c(0, seq(20, 80, length.out = 254), 100),
+	                                            colors = colorRampPalette(col.gc)(256)),
+	                           cluster_rows = FALSE, cluster_columns = FALSE,
+	                           show_row_names = FALSE, show_column_names = FALSE,
+	                           show_heatmap_legend = TRUE,
+	                           heatmap_legend_param = list(color_bar = "continuous"),
+	                           use_raster = use_raster)
 	    L <- c(L, list("percentGC" = hmPercentGC))
 	}
 	ret <- c(L, lapply(which.plots, function(w) {
-		dat <- SummarizedExperiment::assay(x, w)
+		dat <- assay(x, w)
 		if ((w == "enr") | (w == "log2enr")) {
 			rng <- c(-1, 1) * if (is.null(maxEnr)) quantile(abs(dat), .995) else maxEnr
 			cols <- col.enr
@@ -336,17 +357,19 @@ plotMotifHeatmaps <- function(x, which.plots = c("p", "enr", "FDR", "log2enr"), 
 			rng <- c(0, if (is.null(maxSig)) quantile(dat, .995) else maxSig)
 			cols <- col.sig
 		}
-		hm <- ComplexHeatmap::Heatmap(matrix = dat,
-		                              name = assayNameMap1[w],
-		                              width = grid::unit(width,"inch"),
-		                              column_title = assayNameMap2[w],
-		                              col = circlize::colorRamp2(breaks = seq(rng[1], rng[2], length.out = 256),
-		                                                         colors = colorRampPalette(cols)(256)),
-		                              cluster_rows = FALSE, cluster_columns = FALSE, show_row_names = FALSE, show_column_names = FALSE,
-		                              ##column_names_side = "bottom", column_names_max_height = grid::unit(1.5,"inch"),
-		                              top_annotation = hmBin,
-		                              show_heatmap_legend = TRUE, heatmap_legend_param = list(color_bar = "continuous"),
-		                              use_raster = use_raster)
+		hm <- Heatmap(matrix = dat,
+		              name = assayNameMap1[w],
+		              width = unit(width,"inch"),
+		              column_title = assayNameMap2[w],
+		              col = colorRamp2(breaks = seq(rng[1], rng[2], length.out = 256),
+		                               colors = colorRampPalette(cols)(256)),
+		              cluster_rows = FALSE, cluster_columns = FALSE,
+		              show_row_names = FALSE, show_column_names = FALSE,
+		              ##column_names_side = "bottom", column_names_max_height = unit(1.5,"inch"),
+		              top_annotation = hmBin,
+		              show_heatmap_legend = TRUE,
+		              heatmap_legend_param = list(color_bar = "continuous"),
+		              use_raster = use_raster)
 		hm
 	}))
 	names(ret)[seq(length(ret) - length(which.plots) + 1L, length(ret))] <- which.plots
@@ -379,7 +402,8 @@ plotMotifHeatmaps <- function(x, which.plots = c("p", "enr", "FDR", "log2enr"), 
 #' @importFrom graphics matplot
 #'
 #' @export
-plotStabilityPaths <- function(se, selProbMin = metadata(se)$stabsel.params.cutoff,
+plotStabilityPaths <- function(se,
+                               selProbMin = metadata(se)$stabsel.params.cutoff,
                                col = "cadetblue", 
                                lwd = 1, lty = 1, ylim = c(0, 1.1), ...) {
 
@@ -409,70 +433,6 @@ plotStabilityPaths <- function(se, selProbMin = metadata(se)$stabsel.params.cuto
     # return TRUE
     invisible(TRUE)
 }
-
-
-# #' @title Barplot Selection Probabilities
-# #'
-# #' @description Create a bar plot of the selection probabilities in descending order.
-# #'
-# #' @param se the \code{SummarizedExperiment} object resulting from stability selection 
-# #'   (typically a call to \code{\link{randLassoStabSel}}).
-# #' @param selProbMin A numerical scalar in [0,1]. Predictors with a selection
-# #'   probability greater than \code{selProbMin} are shown as colored bars. The
-# #'   color is defined by the \code{col} argument.
-# #' @param ylim the limits for the y-axis.
-# #' @param onlySelected logical (default=TRUE) indicating if only selected predictors' selection probabilities
-# #'   should be plotted.
-# #' @param col color of the selected predictors.
-# #' @param las (2 by default) plot labels vertically or horizontally.
-# #' @param ... additional parameters for the \code{barplot} function.
-# #'
-# #' @seealso \code{\link[graphics]{barplot}}
-# #'
-# #' @return barplot of selection probabilities.
-# #'
-# #' @importFrom SummarizedExperiment assay rowData
-# #' @importFrom graphics barplot
-# #'
-# #' @export
-# plotSelectionProb <- function(se, selProbMin = metadata(se)$stabsel.params.cutoff, 
-#                               ylim = c(0, 1.1), onlySelected = TRUE,
-#                               col = "cadetblue", las = 2, ...) {
-# 
-#     # checks
-#     if (!is(se, "SummarizedExperiment")) {
-#         stop("'se' must be a SummarizedExperiment")
-#     }
-#   
-#     # get selection probabilities (last column; ie last regularization step in assay)
-#     probs  <- se$selProb
-#     names(probs) <- colnames(se)
-#     sel <- probs > selProbMin
-#     cols <- rep("grey", length(probs))
-#     cols[sel] <- col
-#     
-#     
-#     # if onlySelected, only keep selected predictors
-#     if (onlySelected) {
-#         probs <- probs[sel]
-#         cols <- cols[sel]
-#     }
-#     
-#     
-#     # order by selection probability
-#     probs <- sort(probs, decreasing = TRUE)
-#     
-#     
-#     # plot
-#     graphics::barplot(probs, ylim = ylim, ylab = "Selection Probability",
-#                       las = las, col = cols, border = NA, ...)
-#     abline(h = selProbMin, lty = 5, col = "red")
-#     legend("topright", legend = "selProbMin", lty = 5, col = "red", bty = "n")
-#     
-#     
-#     # return TRUE
-#     invisible(TRUE)
-# }
 
 
 #' @title Plot selection probabilities of predictors
