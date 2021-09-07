d6ca5b5a2b7a4d6c8e2b1b5b6f5daa3ddb8e1a8f
Charlotte Soneson <charlottesoneson@gmail.com>
2021-09-07 21:55:50 +0200

    Remove superfluous paste

--- R/motif_enrichment_kmers.R
+++ R/motif_enrichment_kmers.R
@@ -861,9 +861,9 @@ calcBinnedKmerEnr <- function(seqs,
 
             # if df is empty, then all seqs were filtered out in the GC weight calculation step
             if (nrow(df) == 0) {
-                stop(paste0("No sequences remained after the GC weight calculation step in bin ", levels(bins)[i], 
-                            " due to no GC bin containing both fore- and background sequences. ", 
-                            "Cannot proceed with the enrichment analysis ..."))
+                stop("No sequences remained after the GC weight calculation step in bin ", levels(bins)[i], 
+                     " due to no GC bin containing both fore- and background sequences. ", 
+                     "Cannot proceed with the enrichment analysis ...")
             }
 
             # update background sequence weights based on k-mer composition
