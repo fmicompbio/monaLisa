6fda6dac34b9ab76add444d7c49d22a094d91186
Charlotte Soneson <charlottesoneson@gmail.com>
2021-09-09 15:37:55 +0200

    Allow specification of k-mers in motifKmerSimilarity, as well as including reverse complements in the similarity search.

--- tests/testthat/test_pfm_comparison.R
+++ tests/testthat/test_pfm_comparison.R
@@ -63,12 +63,21 @@ test_that("motifSimilarity works as expected", {
     expect_error(motifSimilarity(numeric(0)))
     expect_error(motifSimilarity("error"))
 
-    expect_is(res1 <- motifSimilarity(x = pfmL, y = NULL, method = "R", BPPARAM = BiocParallel::MulticoreParam(3), verbose = TRUE), "matrix")
-    expect_is(res2 <- motifSimilarity(x = pfmL, y = NULL, method = "R", BPPARAM = BiocParallel::MulticoreParam(2), verbose = TRUE), "matrix")
-    expect_is(res3 <- motifSimilarity(x = pfmL, y = pfmL, method = "R", BPPARAM = BiocParallel::MulticoreParam(2), verbose = TRUE), "matrix")
-    expect_is(res4 <- motifSimilarity(x = pfmL, y = pfmL, method = "R", verbose = TRUE), "matrix")
-    expect_is(res5 <- motifSimilarity(x = tmpf, y = NULL, method = "R", verbose = TRUE), "matrix")
-    expect_identical(res1[c(4,7,8)], c(1.0, 0.63146007727884323479, 0.63146007727884323479))
+    expect_is(res1 <- motifSimilarity(x = pfmL, y = NULL, method = "R", 
+                                      BPPARAM = BiocParallel::MulticoreParam(3), 
+                                      verbose = TRUE), "matrix")
+    expect_is(res2 <- motifSimilarity(x = pfmL, y = NULL, method = "R", 
+                                      BPPARAM = BiocParallel::MulticoreParam(2), 
+                                      verbose = TRUE), "matrix")
+    expect_is(res3 <- motifSimilarity(x = pfmL, y = pfmL, method = "R", 
+                                      BPPARAM = BiocParallel::MulticoreParam(2), 
+                                      verbose = TRUE), "matrix")
+    expect_is(res4 <- motifSimilarity(x = pfmL, y = pfmL, method = "R", 
+                                      verbose = TRUE), "matrix")
+    expect_is(res5 <- motifSimilarity(x = tmpf, y = NULL, method = "R", 
+                                      verbose = TRUE), "matrix")
+    expect_identical(res1[c(4,7,8)], c(1.0, 0.63146007727884323479, 
+                                       0.63146007727884323479))
     expect_identical(res1, res2)
     expect_identical(res1, res3)
     expect_identical(res1, res4)
@@ -79,7 +88,9 @@ test_that("motifSimilarity works as expected", {
         homerfile <- findHomer("compareMotifs.pl", dirs = "/work/gbioinfo/Appz/Homer/Homer-4.11/bin")
     }
     if (!is.na(homerfile)) { # only test at home
-        expect_is(res6 <- motifSimilarity(x = tmpf, y = NULL, method = "HOMER", homerfile = homerfile, verbose = TRUE), "matrix")
+        expect_is(res6 <- motifSimilarity(x = tmpf, y = NULL, method = "HOMER", 
+                                          homerfile = homerfile, verbose = TRUE), 
+                  "matrix")
         expect_equal(res1, res6)
     }
 
@@ -94,7 +105,8 @@ test_that("motifKmerSimilarity works as expected", {
     for (i in seq_along(pfmL)) {
         cat(sprintf(">%s\t%s\t5\n", TFBSTools::ID(pfmL[[i]]), TFBSTools::name(pfmL[[i]])),
             file = fh, append = TRUE)
-        write.table(file = fh, t(TFBSTools::Matrix(pfmL[[i]])), row.names = FALSE, col.names = FALSE,
+        write.table(file = fh, t(TFBSTools::Matrix(pfmL[[i]])), 
+                    row.names = FALSE, col.names = FALSE,
                     sep = "\t", quote = FALSE, append = TRUE)
     }
     close(fh)
@@ -106,14 +118,42 @@ test_that("motifKmerSimilarity works as expected", {
     expect_error(motifKmerSimilarity(pfmL, kmerLen = 2.5))
     expect_error(motifKmerSimilarity(pfmL, kmerLen = -3))
 
-    expect_is(res1 <- motifKmerSimilarity(x = pfmL, kmerLen = 4, verbose = TRUE), "matrix")
-    expect_is(res2 <- motifKmerSimilarity(x = tmpf, kmerLen = 4L, BPPARAM = BiocParallel::MulticoreParam(2), verbose = TRUE), "matrix")
+    expect_is(res1 <- motifKmerSimilarity(x = pfmL, kmerLen = 4, 
+                                          verbose = TRUE), "matrix")
+    expect_is(res2 <- motifKmerSimilarity(
+        x = tmpf, kmerLen = 4L, 
+        BPPARAM = BiocParallel::MulticoreParam(2), verbose = TRUE), "matrix")
+    expect_is(res3 <- motifKmerSimilarity(x = pfmL, kmers = FourMers, 
+                                          verbose = TRUE), "matrix")
     expect_identical(res1, res2)
+    expect_identical(res1, res3)
     expect_identical(dim(res1), c(3L, 256L))
     expect_identical(dimnames(res1), list(name(pfmL), FourMers))
     expect_identical(which.max(res1), 2L)
     expect_identical(max(res1), prod(m2[1,-1]))
+    
+    # test with subset of 4-mers
+    FourMersSub <- c("AAAA", "GGCG", "TCGT", "ACTA")
+    FourMersSubRevComp <- c("TTTT", "CGCC", "ACGA", "TAGT")
+    expect_is(res4 <- motifKmerSimilarity(x = pfmL, kmers = FourMersSub,
+                                          verbose = TRUE), "matrix")
+    expect_identical(dim(res4), c(3L, 4L))
+    expect_identical(res1[, FourMersSub], res4)
+    
+    # test with reverse complements
+    expect_is(res5 <- motifKmerSimilarity(x = pfmL, kmers = FourMersSub,
+                                          includeRevComp = TRUE, 
+                                          verbose = TRUE), "matrix")
+    expect_identical(dim(res5), c(3L, 4L))
+    expect_identical(pmax(res1[, FourMersSub], res1[, FourMersSubRevComp]),
+                     res5)
 
+    # test with a single 4-mer
+    expect_is(res6 <- motifKmerSimilarity(x = pfmL, kmers = "ACGT",
+                                          verbose = TRUE), "matrix")
+    expect_identical(dim(res6), c(3L, 1L))
+    expect_identical(res1[, "ACGT", drop = FALSE], res6)
+    
     # clean up
     unlink(x = tmpf)
 })
