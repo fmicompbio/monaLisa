51c702801045a09a6211536678a02d5b58303966
Charlotte Soneson <charlottesoneson@gmail.com>
2025-03-22 10:37:48 +0100

    Add tests for .assertPackagesAvailable

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -96,3 +96,20 @@ test_that(".assertVector works", {
     expect_error(.assertVector(x = test, type = "numeric"),
                  ".test. must be of class .numeric.")
 })
+
+## -------------------------------------------------------------------------- ##
+## Checks, .assertPackagesAvailable
+## -------------------------------------------------------------------------- ##
+test_that(".assertPackagesAvailable works", {
+    testfunc <- function(...) .assertPackagesAvailable(...)
+    expect_error(testfunc(1L))
+    expect_error(testfunc("test", "error"))
+    expect_error(testfunc("test", c(TRUE, FALSE)))
+    
+    expect_true(testfunc("base"))
+    expect_true(testfunc("githubuser/base"))
+    expect_true(testfunc(c("base", "methods")))
+    expect_error(testfunc(c("error", "error2")), "BiocManager")
+    expect_error(testfunc("error1", suggestInstallation = FALSE), "installed.\n$")
+    rm(testfunc)
+})
